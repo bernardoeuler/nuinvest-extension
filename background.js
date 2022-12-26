69f1b87e59459284a3c50dd3e574899a13b03d53
Bernardo Euler <bernardoeulersantos@gmail.com>
2022-12-26 05:38:29 -0300

    Use getStatementBetweenDates() to fetch the statement

--- background.js
+++ background.js
@@ -1,7 +1,6 @@
 import { handleStorageChange } from "./utils/handleStorageChange.js"
 import { storeAuthToken } from "./utils/storeAuthToken.js"
-import { getValidPeriods } from "./utils/getValidPeriods.js"
-import { getData } from "./utils/getData.js"
+import { getStatementBetweenDates } from "./utils/getStatementBetweenDates.js"
 
 const requestFilters = { urls: ["https://*.nuinvest.com.br/api/*"] }
 
@@ -11,27 +10,7 @@ chrome.storage.onChanged.addListener(handleStorageChange)
 
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   sendResponse({received: true})
-
   const { startDate, endDate } = message
-  const { authToken } = await chrome.storage.session.get("authToken")
-  const periods = getValidPeriods(startDate, endDate)
-  const transactions = []
-  
-  for (const period of periods) {
-    const { startDate, endDate } = period
-    const queryString = `startDate=${startDate}&endDate=${endDate}`
-    const statementUrl = `https://www.nuinvest.com.br/api/gringott/statements/1?${queryString}`
-    const periodStatement = await getData(statementUrl, { "Authorization": authToken })
-    if (periodStatement) {
-      transactions.push(...periodStatement.value.statements)
-    }
-  }
-
-  const fullStatement = {
-    transactions,
-    initialBalance: transactions[0].balance - transactions[0].value,
-    finalBalance: transactions[transactions.length - 1].balance
-  }
-
-  console.log(fullStatement)
+  const statement = await getStatementBetweenDates(startDate, endDate)
+  console.log(statement)
 })
