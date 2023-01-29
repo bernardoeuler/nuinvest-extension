544c59bf2d47bdc7459776fb7647e8dcadd1d39f
Bernardo Euler <bernardoeulersantos@gmail.com>
2023-01-28 22:02:18 -0300

    Rename transactions to movimentations

--- utils/getStatementBetweenDates.js
+++ utils/getStatementBetweenDates.js
@@ -4,7 +4,7 @@ import { getValidPeriods } from "./getValidPeriods.js"
 export async function getStatementBetweenDates(startDate, endDate) {
   const { authToken } = await chrome.storage.session.get("authToken")
   const periods = getValidPeriods(startDate, endDate)
-  const transactions = []
+  const movimentations = []
   
   for (const period of periods) {
     const { startDate: currentStartDate, endDate: currentEndDate } = period
@@ -12,13 +12,13 @@ export async function getStatementBetweenDates(startDate, endDate) {
     const statementUrl = `https://www.nuinvest.com.br/api/gringott/statements/1?${queryString}`
     const periodStatement = await getData(statementUrl, { "Authorization": authToken })
     if (periodStatement) {
-      transactions.push(...periodStatement.value.statements)
+      movimentations.push(...periodStatement.value.statements)
     }
   }
 
   return {
-    transactions,
-    initialBalance: transactions[0].balance - transactions[0].value,
-    finalBalance: transactions[transactions.length - 1].balance
+    movimentations,
+    initialBalance: movimentations[0].balance - movimentations[0].value,
+    finalBalance: movimentations[movimentations.length - 1].balance
   }
 }
